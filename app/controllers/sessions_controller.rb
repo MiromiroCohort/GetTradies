fa4ce903cd4f201a337d22e1a8d5fc63501587cb
Gabriel-Bowater <email": null>
2015-08-25 13:56:59 +1200

    add session controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,10 @@
+class SessionsController < ApplicationController
+
+  def new
+    # render text: "good work old bot"
+  end
+
+  def create
+    render text: "wutang clan ain't nothing to fuck with"
+  end
+end
--- spec/features/signups_spec.rb
+++ spec/features/signups_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.feature "Signups", type: :feature do
   scenario "users see signup page" do
-    visit signup_path
+    visit new_session_path
 
     expect(page).to have_selector('form')
   end
