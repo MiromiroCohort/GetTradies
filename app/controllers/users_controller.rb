cf5902e82d21e12d435368d14eeee3dd0cbc4662
Gabriel-Bowater <email": null>
2015-08-27 11:41:31 +1200

    add basic users lists. Add admin rights by profession for users list

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,4 +21,21 @@ class UsersController < ApplicationController
     end
   end
 
+  def index
+    @users = User.all
+  end
+
+  def show
+    # @user = User.find(session[:user_id])
+    if params[:id] != "show"
+      if User.exists?(params[:id])
+        @user = User.find(params[:id])
+      else
+        redirect_to "/"
+      end
+    else
+      @user = User.find(session[:user_id])
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   # You can have the root of your site routed with "root"
-  root 'sessions#new'
+  # root 'sessions#new'
   # get '/signup' => 'signup#new'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
