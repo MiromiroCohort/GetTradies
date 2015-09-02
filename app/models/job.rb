08787cc05201a58f6756ab84f421145ad21cdda6
Gabriel-Bowater <email": null>
2015-09-02 15:53:34 +1200

    refactoring pass 1

--- app/models/job.rb
+++ app/models/job.rb
@@ -8,9 +8,8 @@ class Job < ActiveRecord::Base
 
   has_many :tenders
   belongs_to :user
-  # has_many :tradies, class_name: "User", through: :tenders
-  # , foreign_key: "tradie_id"
+
   def short_desc
-    return description.slice(0,40)
+    return description.truncate(40, separator: ' ')
   end
 end
